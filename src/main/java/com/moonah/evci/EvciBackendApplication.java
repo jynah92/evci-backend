ce626b97eba3407a19baa9829d273006163ff890
jynah92 <jynah92@kakao.com>
2022-01-30 15:56:32 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/moonah/evci/EvciBackendApplication.java
@@ -0,0 +1,13 @@
+package com.moonah.evci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvciBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvciBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moonah/evci/EvciBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moonah.evci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvciBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
